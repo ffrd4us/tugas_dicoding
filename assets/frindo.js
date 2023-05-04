3119942ea632558a98a0ec7810831f3f17b205c3
Firdaus <ffrd4us@gmail.com>
2023-05-04 20:08:56 +0700

    all

---
+++ assets/frindo.js
@@ -0,0 +1,37 @@
+/* overlay navbar */
+function navigasiOpen() {
+    document.getElementById("simple_nav").style.width = "100%";
+}
+
+function navigasiClose() {
+    document.getElementById("simple_nav").style.width = "0";
+}
+
+/* overlay tombol info */
+function navigasiOpenInfo() {
+    document.getElementById("info_button").style.width = "100%";
+}
+
+function navigasiCloseInfo() {
+    document.getElementById("info_button").style.width = "0";
+}
+
+/* overlay tombol mulai >> */
+
+function navigasiOpenMulai() {
+    document.getElementById("mulai_button").style.width = "100%";
+}
+
+function navigasiCloseMulai() {
+    document.getElementById("mulai_button").style.width = "0";
+}
+
+/* overlay tombol login */
+
+function navigasiOpenLogin() {
+    document.getElementById("login_button").style.width = "100%";
+}
+
+function navigasiCloseLogin() {
+    document.getElementById("login_button").style.width = "0";
+}
